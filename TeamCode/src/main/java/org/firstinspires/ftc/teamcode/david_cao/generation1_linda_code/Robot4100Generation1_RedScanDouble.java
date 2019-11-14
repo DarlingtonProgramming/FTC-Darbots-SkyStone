3c7a8950a6a0d5d5d9e3a2b9a3d69808ec40c407
年迈的老秋风Windy <cxcyhdyx@sina.com>
2019-11-14 17:38:46 -0500

    Optimize Scan Double

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/david_cao/generation1_linda_code/Robot4100Generation1_RedScanDouble.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/david_cao/generation1_linda_code/Robot4100Generation1_RedScanDouble.java
@@ -166,12 +166,14 @@ public class Robot4100Generation1_RedScanDouble extends DarbotsBasicOpMode<Robot
             return;
         }
 
+        double thirdResultOffset = this.ScanResult == 3 ? -3 : 0;
+
         this.getRobotCore().getChassis().addTask(this.getRobotCore().getChassis().getFixedZDistanceTask(
                 60,
                 0.2
         ));
         this.getRobotCore().getChassis().addTask(this.getRobotCore().getChassis().getFixedZDistanceTask(
-                -(2*Robot4100Generation1_Settings.AUTONOMOUS_LENGTH_FOR_EACH_STONE - (this.ScanResult - 1) * Robot4100Generation1_Settings.AUTONOMOUS_LENGTH_FOR_EACH_STONE),
+                -(2*Robot4100Generation1_Settings.AUTONOMOUS_LENGTH_FOR_EACH_STONE - ((this.ScanResult - 1) * Robot4100Generation1_Settings.AUTONOMOUS_LENGTH_FOR_EACH_STONE) + thirdResultOffset + 3),
                 0.25
         ));
         this.getRobotCore().getChassis().addTask(this.getRobotCore().getChassis().getFixedXDistanceTask(
