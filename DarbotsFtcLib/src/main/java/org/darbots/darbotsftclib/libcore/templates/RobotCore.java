e4f3168c2a8be8f99a08d2d10468ea25b47ddefe
Windy Feng <1745500559@qq.com>
2019-12-04 22:50:37 -0500

    Add Field Oriented Control + Tester

--- DarbotsFtcLib/src/main/java/org/darbots/darbotsftclib/libcore/templates/RobotCore.java
+++ DarbotsFtcLib/src/main/java/org/darbots/darbotsftclib/libcore/templates/RobotCore.java
@@ -80,6 +80,9 @@ public abstract class RobotCore implements RobotNonBlockingDevice {
     @Override
     public void updateStatus(){
         this.m_UpdateStatusCount++;
+        if(this.m_Gyro instanceof RobotNonBlockingDevice){
+            ((RobotNonBlockingDevice) this.m_Gyro).updateStatus();
+        }
         this.__updateStatus();
     }
     public long getUpdateStatusCount(){
