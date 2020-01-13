92b11d044a18aaa6d59a7764958392bd8096f820
Windy Feng <1745500559@qq.com>
2020-01-13 13:18:02 -0500

    Update SwanSilverChassisTuning.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/david_cao/Gen4_SwanSilver_Code/SwanSilverChassisTuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/david_cao/Gen4_SwanSilver_Code/SwanSilverChassisTuning.java
@@ -79,7 +79,7 @@ public class SwanSilverChassisTuning extends DarbotsBasicOpMode<SwanSilverCore> 
         }
         double endXOffset = this.getRelativeOffset().X;
         telemetry.addData("Info","End of X factor tuning");
-        telemetry.addData("X Factor","Actual Distance Pushed / " + endXOffset);
+        telemetry.addData("X Factor","" + endXOffset + " / Actual Distance Pushed");
         telemetry.addData("Info", "Press X to continue");
         telemetry.update();
         if(!waitForPadX()){
@@ -105,7 +105,7 @@ public class SwanSilverChassisTuning extends DarbotsBasicOpMode<SwanSilverCore> 
         }
         double endYOffset = this.getRelativeOffset().Y;
         telemetry.addData("Info","End of Y factor tuning");
-        telemetry.addData("Y Factor","Actual Distance Pushed / " + endYOffset);
+        telemetry.addData("Y Factor","" + endYOffset + " / Actual Distance Pushed");
         telemetry.addData("Info","Press X on gamepad to continue");
         telemetry.update();
         if(!waitForPadX()){
@@ -131,7 +131,7 @@ public class SwanSilverChassisTuning extends DarbotsBasicOpMode<SwanSilverCore> 
         }
         double endZRot = this.getRelativeOffset().getRotationZ();
         telemetry.addData("Info","End of Z Rot factor tuning");
-        telemetry.addData("Z Rot Factor","Actual Deg Pushed / " + endZRot);
+        telemetry.addData("Z Rot Factor","" + endZRot + " / Actual Deg Pushed");
         telemetry.addData("Info","Congrats, Tuning is done!");
         telemetry.update();
         while(this.opModeIsActive()){
