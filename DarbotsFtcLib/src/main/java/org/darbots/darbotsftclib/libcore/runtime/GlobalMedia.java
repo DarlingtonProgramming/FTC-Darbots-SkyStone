8da977a007f9b7d71bca36d349397a4f5d2cefea
年迈的老秋风Windy <cxcyhdyx@sina.com>
2020-02-04 13:50:37 -0500

    Add GlobalMedia Player

---
+++ DarbotsFtcLib/src/main/java/org/darbots/darbotsftclib/libcore/runtime/GlobalMedia.java
@@ -0,0 +1,58 @@
+package org.darbots.darbotsftclib.libcore.runtime;
+
+import android.content.Context;
+
+import com.qualcomm.ftccommon.SoundPlayer;
+
+import java.io.File;
+
+public class GlobalMedia {
+
+    /**
+     * Play a resource file in an Android Module
+     * @param context the context (module) pointer, you can use OpMode.appContext as the TeamCode Module pointer
+     * @param waitForPreviousSound whether to wait for any currently-playing non-looping sound to finish before playing
+     * @param resourceID the ID of the resource. You can use R.raw.ResourceFileName to get the ID of the resource
+     * @param rate the speed the sound is played [0.5 - 2.0]
+     * @param volume the volume scaling that will be applied to the sound
+     * @param loopSound are we looping the music
+     * @param loopTime number of time the sound gets played, -1 = forever.
+     */
+    public void playResourceFile(Context context, boolean waitForPreviousSound, int resourceID, float rate, float volume, boolean loopSound, int loopTime){
+        int realLoopTime = loopSound ? (loopTime == -1 ? -1 : loopTime - 1) : 0;
+        SoundPlayer.PlaySoundParams params = new SoundPlayer.PlaySoundParams();
+        params.loopControl = realLoopTime;
+        params.volume = volume;
+        params.waitForNonLoopingSoundsToFinish = waitForPreviousSound;
+        params.rate = rate;
+        SoundPlayer.getInstance().startPlaying(context,resourceID,params,null,null);
+    }
+
+    /**
+     * Play a resource file in an Android Module
+     * @param context the context (module) pointer, you can use OpMode.appContext as the TeamCode Module pointer
+     * @param waitForPreviousSound whether to wait for any currently-playing non-looping sound to finish before playing
+     * @param soundFile The file pointer pointing to the sound file
+     * @param rate the speed the sound is played [0.5 - 2.0]
+     * @param volume the volume scaling that will be applied to the sound
+     * @param loopSound are we looping the music
+     * @param loopTime number of time the sound gets played, -1 = forever.
+     */
+    public void playLocalFile(Context context, boolean waitForPreviousSound, File soundFile, float rate, float volume, boolean loopSound, int loopTime){
+        int realLoopTime = loopSound ? (loopTime == -1 ? -1 : loopTime - 1) : 0;
+        SoundPlayer.PlaySoundParams params = new SoundPlayer.PlaySoundParams();
+        params.loopControl = realLoopTime;
+        params.volume = volume;
+        params.waitForNonLoopingSoundsToFinish = waitForPreviousSound;
+        params.rate = rate;
+        SoundPlayer.getInstance().startPlaying(context,soundFile,params,null,null);
+    }
+
+    public void stopPlayingAllSounds(){
+        SoundPlayer.getInstance().stopPlayingAll();
+    }
+
+    public void stopPlayingLoopSounds(){
+        SoundPlayer.getInstance().stopPlayingLoops();
+    }
+}
