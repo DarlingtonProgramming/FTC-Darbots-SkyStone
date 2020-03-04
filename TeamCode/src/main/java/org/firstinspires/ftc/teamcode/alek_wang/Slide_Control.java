9ed8dffc2d529d5da305a376e17b5bdb67cfae97
Alek <316559928@qq.com>
2020-03-04 17:40:22 -0500

    Update Slide_Control.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/alek_wang/Slide_Control.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/alek_wang/Slide_Control.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.alek_wang;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.darbots.darbotsftclib.libcore.motortypes.GoBilda5202Series1150RPMMotor;
 import org.darbots.darbotsftclib.libcore.templates.motor_related.MotorType;
@@ -12,22 +13,41 @@ public class Slide_Control extends LinearOpMode {
     private DcMotor slidemotor = null;
     private MotorType slidetype = new GoBilda5202Series1150RPMMotor();
     private int low, high;
-    private int count;
+    private int count = 1900;
 
 
     @Override
     public void runOpMode() throws InterruptedException {
-        slidemotor = hardwareMap.get(DcMotor.class, "slidemotor");
+        slidemotor = hardwareMap.get(DcMotor.class, "stackerSlideMotor");
+
+        slidemotor.setDirection(DcMotor.Direction.REVERSE);
+        slidemotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         low = slidemotor.getCurrentPosition();
         high = low + count;
+
+        waitForStart();
 
 
         while (opModeIsActive()){
             telemetry.addData("Ticks", slidemotor.getCurrentPosition());
             telemetry.update();
 
-            if(gamepad1.left_stick_y != 0){
-                slidemotor.setPower(gamepad1.left_stick_y * 0.3);
+            double power = -gamepad1.left_stick_y;
+
+            if(power != 0){
+                if(slidemotor.getCurrentPosition() >= low && slidemotor.getCurrentPosition() <= high){
+                    slidemotor.setPower(power * 0.8);
+                }
+                else if(slidemotor.getCurrentPosition() < low && power > 0){
+                    slidemotor.setPower(power);
+
+                }
+                else if(slidemotor.getCurrentPosition() > high && power < 0){
+                    slidemotor.setPower(power);
+
+                }else{
+                    slidemotor.setPower(0);
+                }
             }else{
                 slidemotor.setPower(0);
             }
